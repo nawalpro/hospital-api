d6f9f3348a018eab78bc9712adaec7ffa8a7660d
nawalpro <nawal.maizate@gmail.com>
2021-12-22 22:11:14 +0100

    wip

--- clientHospital/src/pages/LoginPage.js
+++ clientHospital/src/pages/LoginPage.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function LoginPage() {
+  
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+
+const handleChange = (e) => {
+  e.preventDefault();
+  axios({
+    method: 'post',
+    url: 'http://localhost:8000/patient/auth',
+    withcredentials: true,
+    data: {
+        email,
+        password,
+    },
+   
+})
+  
+} 
     return (
         <div>
            <form>
-            <input type="text" value='email'/>
-            
-            <input type="password" value='password' />
-            
-            <button></button>
-            
-        
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="submit" value='Valider' onChange={handleChange} />
             <button class="create-account">Create New Account</button>
           </form> 
         </div>
