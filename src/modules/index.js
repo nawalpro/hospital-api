b47eb38f1c3a367f7a802388b21ebc4bb1298a02
jerome.eliezer-vanerot <jerome.eliezer-vanerot@o4software.com>
2021-12-23 22:38:12 +0100

    clean commit because refacto Architecture :fire:

--- src/modules/index.js
+++
@@ -1,6 +0,0 @@
-const ServiceRouter = require('./Service/ServiceRouter')
-const PatientRouter = require('./Patient/PatientRouter')
-
-const routes = [ ServiceRouter, PatientRouter ];
-
-module.exports = routes;
