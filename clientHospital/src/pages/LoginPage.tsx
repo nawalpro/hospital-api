ad1ac17cddcb4e50091f53c311d0d00489521f6c
jerome.eliezer-vanerot <jerome.eliezer-vanerot@o4software.com>
2022-01-23 19:31:48 +0100

    jev in back'

--- clientHospital/src/pages/LoginPage.tsx
+++ clientHospital/src/pages/LoginPage.tsx
@@ -1,112 +1,25 @@
-import React, { FC, useState, FormEvent } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-
-const sendForm = async (event: FormEvent<HTMLFormElement>) => {
-	event.preventDefault();
-
-	const { email, password } = event.target as typeof event.target & {
-		email: { value: string };
-		password: { value: string };
-		error: { value: string};
-	};
-
-	console.log("email", email.value);
-	console.log("password", password.value);
-  
-    const response = await axios.post("http://localhost:8000/patient/auth", {email, password});
-	console.log("response", response)
-   
-	
-	// await fetch ("http://localhost:8000/patient/auth", {
-		
-	// 	method: "POST",
-	// 	body: JSON.stringify({
-	// 		email:email.value,
-	// 		password: password.value,
-	// 	})
+import React from 'react'
 
 
 
-function LoginPage() {
+interface userlogin {
+	email?: string;
+	password?: string;
+	phone?: string;
+	adress?: string;
+	lastName?: string;
+	firstName?: string;
+    error?: string;
+};
+
+const LoginPage = () => {
 	return (
-		<form onSubmit={sendForm}>
-			<fieldset>
-				<label htmlFor='email'>Mail</label>
-				<input type='text' id='email' />
-			</fieldset>
-			<fieldset>
-				<label htmlFor='password'>Password</label>
-				<input type='password' id='password' />
-			</fieldset>
-			<button type='submit'>Login</button>
-		</form>
-	);
+		<div>
+			<form>
+
+			</form>
+		</div>
+	)
 }
 
-export default LoginPage;
-
-// interface userLogin {
-// 	email?: string;
-// 	password?: string;
-//     error?: string;
-// };
-
-// const LoginPage: FC<userLogin> = () => {
-//  const [email, setEmail] = useState('')
-//  const [password, setPassword] = useState('')
-//  const [error, setError] = useState('')
-
-// 	return (
-// 		<>
-
-// 			<section className='login'>
-// 				<h2>Se connecter</h2>
-// 				{error && <p className='error'>{error}</p>}
-// 				<form>
-// 					<div className='form-group'>
-// 						<label htmlFor='email'>
-// 							Email<span className='required'></span>
-// 						</label>
-// 						<input
-// 							type='email'
-// 							className='form-control'
-// 							required
-// 							onChange={(e) => setEmail(e.target.value)}
-// 						/>
-// 					</div>
-// 					<div className='form-group'>
-// 						<label htmlFor='password'>
-// 							Mot de passe<span className='required'></span>
-// 						</label>
-// 						<input
-// 							type='password'
-// 							className='form-control'
-// 							required
-// 							onChange={(e) => setPassword(e.target.value)}
-// 						/>
-// 					</div>
-
-// 					<div className='form-group'>
-// 						<button  className='mainbutton'>
-// 							Valider
-// 						</button>
-// 					</div>
-
-// 					<div className='form-group'>
-// 						<p>
-// 							Vous n’avez pas de compte ?<Link to='/signup'> Cliquez ici.</Link>
-// 						</p>
-// 						<p>
-// 							Tous les champs indiqués par un "
-// 							<span className='required'>*</span>" sont obligatoires. Merci de
-// 							remplir le formulaire.
-// 						</p>
-// 					</div>
-// 				</form>
-// 			</section>
-// 		</>
-// 	);
-// }
-
-// export default LoginPage;
+export default LoginPage
