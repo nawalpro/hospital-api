c120c6ad525351ea69f1e6e7e0bab41cbf4b2441
RebeccaKanu <kanurebecca0@gmail.com>
2022-04-12 11:44:43 +0200

    patient module in progress

--- apiHospital/src/modules/Patient/PatientEntity.ts
+++ apiHospital/src/modules/Patient/PatientEntity.ts
@@ -6,17 +6,12 @@ import {
   BaseEntity,
   ManyToMany
 } from "typeorm";
-import { Role } from "../Role/RoleEntity";
 import { User } from "../User/UserEntity";
 
 @Entity()
 export class Patient extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: any;
-
-  @ManyToMany(() => Role,
-  role => role.id)
-  role!: Role[];
   
   @ManyToMany(() => User,
   user => user.id)
