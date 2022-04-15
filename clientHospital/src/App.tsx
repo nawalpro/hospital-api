cf5f8116f6fc5a4c84d35e079ee525f8ed71ccb0
Jerome <jerome@MacBook-Pro-de-Jerome.local>
2022-04-15 21:53:29 +0200

    home Page in progress

--- clientHospital/src/App.tsx
+++ clientHospital/src/App.tsx
@@ -1,32 +1,13 @@
 import RoutePath from './routes/index';
-import * as React from 'react';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import React from 'react';
+import { Container, AppBar, Toolbar, Typography, Button, Grid, Grow } from '@material-ui/core';
 
 function App() {
-  const [mode, setMode] = React.useState<PaletteMode>('light');
-  const colorMode = React.useMemo(
-    () => ({
-      // The dark mode switch would invoke this method
-      toggleColorMode: () => {
-        setMode((prevMode: PaletteMode) =>
-          prevMode === 'light' ? 'dark' : 'light',
-        );
-      },
-    }),
-    [],
-  );
-  // Update the theme only if the mode changes
-  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  return (
+    <Container maxWidth="lg">
+                   <RoutePath />
 
-  return ( 
-    <ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    <RoutePath/>
-    </ThemeProvider>
-    </ColorModeContext.Provider>
+    </Container>
   )
 }
 
--- clientHospital/src/pages/HomePage.tsx
+++ clientHospital/src/pages/HomePage.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Container, AppBar, Toolbar, Typography, Button, Grid, Grow } from '@material-ui/core';
+import weCare from '../images/weCareBlue.png';
+import Login from '../components/logs/login';
+
+
 export default function HomePage() {
     return (
-        <div>
-            <p>...in progress</p>
-            <Link to="/login">Connect</Link>
-        </div>
+        <Container maxWidth="lg">
+      <AppBar position="static">
+        <Typography variant="h2" align='center'>We-Care</Typography>
+        <img src={weCare} alt="weCare" height='60' />
+      </AppBar>
+      <Grow in >
+        <Container>
+          <Grid container justify= 'space-between' alignItems='stretch' spacing={4} >
+              <Grid item xs={12} sm={7}>
+                <Typography variant="h4" align='center'>
+                    We-Care est une plateform qui connecte les profesionnels de santé au patient.
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+
+    </Container>
     )
 }
